cf1fd777a52b53e5a90d4ac776675e8eba011f02
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-30 18:39:43 +0300

    add task 64

---
+++ task64/Program.cs
@@ -0,0 +1,9 @@
+﻿string list(int n)
+{
+    if (n == 1)
+         return "1 ";
+    return $"{n} " + list(n - 1);
+}
+Console.Write("Insert the number: ");
+int n = Convert.ToInt32(Console.ReadLine());                                          
+Console.Write(list(n)); 
