875db999900a9af5882232e6c081a8d7a9d96fed
Mary Nudnova <m.nudnova@yandex.ru>
2023-10-30 19:56:29 +0300

    add task 66

---
+++ task66/Program.cs
@@ -0,0 +1,13 @@
+﻿// summa between
+int sum(int m, int n)
+{
+    if(m == n)
+        return m;
+    return sum(m, n - 1) + m; // я торможу и не могу понять как сделать... 
+}
+
+Console.Write("Insert the starting number: "); 
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Insert the last number: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(sum(m, n));
